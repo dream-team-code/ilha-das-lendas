4b3d78ca3aa3b0ec8938f8fc418b801027e8fdb4
Gusta-Alves <gustavoalves1010@gmail.com>
2023-02-11 09:34:29 -0300

    create teams component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'times', loadChildren: () => import('./pages/teams/teams.module').then(m => m.TeamsModule) }
 ];
 
 @NgModule({
