6440d1777dff5cd21e84644307a1f414b5ade6c4
Gusta-Alves <gustavoalves1010@gmail.com>
2023-02-12 17:52:17 -0300

    att teams

--- src/app/services/team-mock.service.ts
+++ src/app/services/team-mock.service.ts
@@ -14,91 +14,91 @@ export class TeamMockService {
             {
                 id: "1",
                 nome: "Red Canids",
-                media: 94,               
+                media: 89,               
                 jogadores: [],
                 caminhoRelativo: "red.png",
                 caminhoAbsoluto: "red.png",
                 status: "Ativo"
             },
             {
-                id: "2",
-                nome: "Pain Gaming",
-                media: 88,               
-                jogadores: [],
-                caminhoRelativo: "pain.png",
-                caminhoAbsoluto: "pain.png",
-                status: "Ativo"
-            },
-            {
                 id: "3",
                 nome: "Los Grandes",
-                media: 83,               
+                media: 88,               
                 jogadores: [],
                 caminhoRelativo: "losgrandes.png",
                 caminhoAbsoluto: "losgrandes.png",
                 status: "Ativo"
             },
             {
-                id: "4",
-                nome: "INTZ",
-                media: 80,               
-                jogadores: [],
-                caminhoRelativo: "intz.png",
-                caminhoAbsoluto: "intz.png",
-                status: "Ativo"
-            },
-            {
                 id: "5",
                 nome: "LOUD",
-                media: 78,               
+                media: 86,               
                 jogadores: [],
                 caminhoRelativo: "loud.png",
                 caminhoAbsoluto: "loud.png",
                 status: "Ativo"
             },
             {
-                id: "6",
-                nome: "Vivo Keyd",
-                media: 76,               
-                jogadores: [],
-                caminhoRelativo: "keyd.png",
-                caminhoAbsoluto: "keyd.png",
-                status: "Ativo"
-            },
-            {
-                id: "7",
-                nome: "Fluxo",
-                media: 70,               
-                jogadores: [],
-                caminhoRelativo: "fluxo.png",
-                caminhoAbsoluto: "fluxo.png",
-                status: "Ativo"
-            },
-            {
-                id: "8",
-                nome: "Liberty",
-                media: 68,               
-                jogadores: [],
-                caminhoRelativo: "liberty.png",
-                caminhoAbsoluto: "liberty.png",
-                status: "Ativo"
-            },
-            {
                 id: "9",
                 nome: "Kabum",
-                media: 65,               
+                media: 85,               
                 jogadores: [],
                 caminhoRelativo: "kabum.png",
                 caminhoAbsoluto: "kabum.png",
                 status: "Ativo"
             },
             {
+                id: "7",
+                nome: "Fluxo",
+                media: 79,               
+                jogadores: [],
+                caminhoRelativo: "fluxo.png",
+                caminhoAbsoluto: "fluxo.png",
+                status: "Ativo"
+            },
+            {
+                id: "4",
+                nome: "INTZ",
+                media: 77,               
+                jogadores: [],
+                caminhoRelativo: "intz.png",
+                caminhoAbsoluto: "intz.png",
+                status: "Ativo"
+            },
+            {
                 id: "10",
                 nome: "FURIA",
-                media: 63,               
+                media: 75,               
                 jogadores: [],
                 caminhoRelativo: "furia.png",
                 caminhoAbsoluto: "furia.png",
+                status: "Ativo"
+            },
+            {
+                id: "2",
+                nome: "Pain Gaming",
+                media: 73,               
+                jogadores: [],
+                caminhoRelativo: "pain.png",
+                caminhoAbsoluto: "pain.png",
+                status: "Ativo"
+            },
+            {
+                id: "8",
+                nome: "Liberty",
+                media: 72,               
+                jogadores: [],
+                caminhoRelativo: "liberty.png",
+                caminhoAbsoluto: "liberty.png",
+                status: "Ativo"
+            },
+            {
+                id: "6",
+                nome: "Vivo Keyd",
+                media: 68,               
+                jogadores: [],
+                caminhoRelativo: "keyd.png",
+                caminhoAbsoluto: "keyd.png",
                 status: "Ativo"
             },
         ],
