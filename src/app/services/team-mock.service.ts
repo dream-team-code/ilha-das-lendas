474662f3164335f434711394a2877c6467815fdf
Gusta-Alves <gustavoalves1010@gmail.com>
2023-02-12 22:10:54 -0300

    att teams classification

--- src/app/services/team-mock.service.ts
+++ src/app/services/team-mock.service.ts
@@ -12,36 +12,27 @@ export class TeamMockService {
     private teams: Response<Time> = {
         pagina: [
             {
-                id: "1",
-                nome: "Red Canids",
-                media: 89,               
-                jogadores: [],
-                caminhoRelativo: "red.png",
-                caminhoAbsoluto: "red.png",
-                status: "Ativo"
-            },
-            {
                 id: "3",
                 nome: "Los Grandes",
-                media: 88,               
+                media: 90,               
                 jogadores: [],
                 caminhoRelativo: "losgrandes.png",
                 caminhoAbsoluto: "losgrandes.png",
                 status: "Ativo"
             },
             {
-                id: "5",
-                nome: "LOUD",
-                media: 86,               
+                id: "1",
+                nome: "Red Canids",
+                media: 88,               
                 jogadores: [],
-                caminhoRelativo: "loud.png",
-                caminhoAbsoluto: "loud.png",
+                caminhoRelativo: "red.png",
+                caminhoAbsoluto: "red.png",
                 status: "Ativo"
             },
             {
                 id: "9",
                 nome: "Kabum",
-                media: 85,               
+                media: 86,               
                 jogadores: [],
                 caminhoRelativo: "kabum.png",
                 caminhoAbsoluto: "kabum.png",
@@ -50,16 +41,25 @@ export class TeamMockService {
             {
                 id: "7",
                 nome: "Fluxo",
-                media: 79,               
+                media: 82,               
                 jogadores: [],
                 caminhoRelativo: "fluxo.png",
                 caminhoAbsoluto: "fluxo.png",
                 status: "Ativo"
             },
             {
+                id: "5",
+                nome: "LOUD",
+                media: 81,               
+                jogadores: [],
+                caminhoRelativo: "loud.png",
+                caminhoAbsoluto: "loud.png",
+                status: "Ativo"
+            },
+            {
                 id: "4",
                 nome: "INTZ",
-                media: 77,               
+                media: 76,               
                 jogadores: [],
                 caminhoRelativo: "intz.png",
                 caminhoAbsoluto: "intz.png",
@@ -68,34 +68,34 @@ export class TeamMockService {
             {
                 id: "10",
                 nome: "FURIA",
-                media: 75,               
+                media: 76,               
                 jogadores: [],
                 caminhoRelativo: "furia.png",
                 caminhoAbsoluto: "furia.png",
                 status: "Ativo"
             },
             {
-                id: "2",
-                nome: "Pain Gaming",
-                media: 73,               
-                jogadores: [],
-                caminhoRelativo: "pain.png",
-                caminhoAbsoluto: "pain.png",
-                status: "Ativo"
-            },
-            {
                 id: "8",
                 nome: "Liberty",
-                media: 72,               
+                media: 74,               
                 jogadores: [],
                 caminhoRelativo: "liberty.png",
                 caminhoAbsoluto: "liberty.png",
                 status: "Ativo"
             },
             {
+                id: "2",
+                nome: "Pain Gaming",
+                media: 72,               
+                jogadores: [],
+                caminhoRelativo: "pain.png",
+                caminhoAbsoluto: "pain.png",
+                status: "Ativo"
+            },
+            {
                 id: "6",
                 nome: "Vivo Keyd",
-                media: 68,               
+                media: 62,               
                 jogadores: [],
                 caminhoRelativo: "keyd.png",
                 caminhoAbsoluto: "keyd.png",
